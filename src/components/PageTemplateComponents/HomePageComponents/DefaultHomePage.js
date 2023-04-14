d49c355139c5a0f26da9a8ab66aa0887194fccee
YonatanLahav <Jonatan.Lahav@gmail.com>
2023-04-14 17:13:09 +0300

    fix homePage api

--- src/components/PageTemplateComponents/HomePageComponents/DefaultHomePage.js
+++ src/components/PageTemplateComponents/HomePageComponents/DefaultHomePage.js
@@ -1,21 +1,41 @@
-import React, { useContext } from 'react'
-import { Container, Paper, Grid, Divider } from '@mui/material';
-import HeaderCard from './HeaderCard';
-import UserContext from '../../../context/UserContext';
+import React from "react";
+import { Container, Paper, Grid, Divider } from "@mui/material";
+import HeaderCard from "./HeaderCard";
+import VacationCard from "./VacationCard";
+import HoverShadowCard from "./HoverShadowCard";
 
-function DefaultHomePage({ vacationCards, setActivePage }) {
+function DefaultHomePage({ plans, setActivePage, setVacationIndex }) {
     return (
-        <Container maxWidth="lg" sx={{ mt: 6, mb: 4, }}>
+        <Container maxWidth="lg" sx={{ mt: 6, mb: 4 }}>
             <Grid container spacing={3}>
-                <Grid item xs={12} >
-                    <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                <Grid item xs={12}>
+                    <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
                         <HeaderCard setActivePage={setActivePage} />
                     </Paper>
                 </Grid>
-                {vacationCards}
+                {plans.map((plan, index) => (
+                    <Grid
+                        item
+                        xs={4}
+                        sx={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                    >
+                        <HoverShadowCard
+                            element={
+                                <VacationCard
+                                    vacation={plan}
+                                    onClick={() => setVacationIndex(index)}
+                                />
+                            }
+                        />
+                    </Grid>
+                ))}
             </Grid>
         </Container>
-    )
+    );
 }
 
-export default DefaultHomePage
+export default DefaultHomePage;
