2faf0f36a8f49c740cff457cf67cbece8428d44e
YonatanLahav <Jonatan.Lahav@gmail.com>
2023-06-02 11:24:11 +0300

    Info

--- src/components/PageTemplateComponents/SettingsComponents/info.js
+++ src/components/PageTemplateComponents/SettingsComponents/info.js
@@ -11,44 +11,44 @@ import ApartmentImageStepper from '../HomePageComponents/ApartmentImageStepper';
 
 
 function Info() {
-    const {userData}  = useContext(UserContext);
+  const { userData } = useContext(UserContext);
   return (
     <TableContainer component={Paper}>
-    <Table sx={{ minWidth: 650 }} aria-label="simple table">
-      <TableHead>
-      <TableRow>
-          <TableCell>First Name</TableCell>
-          <TableCell align="right">{JSON.parse(userData).firstName}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>Last Name</TableCell>
-          <TableCell align="right">{JSON.parse(userData).lastName}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>Country</TableCell>
-          <TableCell align="right">{JSON.parse(userData).apartment.country}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>Region</TableCell>
-          <TableCell align="right">{JSON.parse(userData).apartment.region}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>City</TableCell>
-          <TableCell align="right">{JSON.parse(userData).apartment.city}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>Number of rooms</TableCell>
-          <TableCell align="right">{(JSON.parse(userData).apartment.rooms)}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>Number of bathrooms</TableCell>
-          <TableCell align="right">{JSON.parse(userData).apartment.bathrooms}</TableCell>
-        </TableRow>
-      </TableHead>
-    </Table> 
-    <TableCell>Pictures</TableCell>
-    <ApartmentImageStepper images={(JSON.parse(userData).apartment.pictures)} />
-  </TableContainer>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>First Name</TableCell>
+            <TableCell align="right">{JSON.parse(userData).firstName}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Last Name</TableCell>
+            <TableCell align="right">{JSON.parse(userData).lastName}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Country</TableCell>
+            <TableCell align="right">{JSON.parse(userData).apartment.country}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Region</TableCell>
+            <TableCell align="right">{JSON.parse(userData).apartment.region}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>City</TableCell>
+            <TableCell align="right">{JSON.parse(userData).apartment.city}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Number of rooms</TableCell>
+            <TableCell align="right">{(JSON.parse(userData).apartment.rooms)}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Number of bathrooms</TableCell>
+            <TableCell align="right">{JSON.parse(userData).apartment.bathrooms}</TableCell>
+          </TableRow>
+        </TableHead>
+      </Table>
+      <TableCell>Pictures</TableCell>
+      <ApartmentImageStepper images={(JSON.parse(userData).apartment.pictures)} />
+    </TableContainer>
 
   )
 }
